06a36cd54ab083db6f798ae476f954aa73783405
DIS\LABSTUD23 <rnelson3@bowdoin.edu>
2026-09-17 13:39:58 +0200

    Add blank item empty prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed=5f;
+    
+    [Header("Up Sprites")]
+    [SerializeField] private Sprite up1;
+    [SerializeField] private Sprite up2;
+
+    [Header("Down Sprites")]
+    [SerializeField] private Sprite Down1;
+    [SerializeField] private Sprite down2;
+   
+    [Header("Left Sprites")]
+    [SerializeField] private Sprite Left1;
+    [SerializeField] private Sprite Left2;
+
+    [Header("Right Sprites")]
+    [SerializeField] private Sprite Right1;
+    [SerializeField] private Sprite Right2;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        spriteRenderer=GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMove(InputValue input)
+    {
+        movement=input.Get<Vector2>();
+        UpdateSprite();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity=movement*moveSpeed;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

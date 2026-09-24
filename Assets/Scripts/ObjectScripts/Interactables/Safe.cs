a904bb115e87074e88b27b0f659eee0e01381159
rnelson2027 <rnelson3@bowdoin.edu>
2026-09-24 17:46:38 +0200

    Mostly done with level 4

---
+++ Assets/Scripts/ObjectScripts/Interactables/Safe.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Safe : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject[] hiddenGameObjects;
+    [SerializeField] private Sprite openSafe;
+
+    [Header("Safe's lock GameObject")]
+    [SerializeField] private GameObject safeLock;
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        foreach (GameObject hiddenObject in hiddenGameObjects)
+        {
+            hiddenObject.GetComponent<IHidden>().Hide();
+        }
+    }
+
+    public void Interact()
+    {
+        if (safeLock == null)
+        {
+            Debug.Log("safe lock not assigned");
+            return;
+           
+        }
+
+        Lock safelock = safeLock.GetComponent<Lock>();
+        if (safelock.IsUnlocked())
+        {
+            Debug.Log("safe unlocked");
+            foreach (GameObject hiddenObject in hiddenGameObjects)
+            {
+                hiddenObject.GetComponent<IHidden>().Show();
+            }
+            sr.sprite = openSafe;
+        }
+    }
+}

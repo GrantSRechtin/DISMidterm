49673002e3cdb1bba48a5dd789e561b68cd8cc17
rnelson2027 <rnelson3@bowdoin.edu>
2026-09-24 18:42:12 +0200

    Finish level 4 and make chest lids interactable only once

--- Assets/Scripts/ObjectScripts/Interactables/Chest.cs
+++ Assets/Scripts/ObjectScripts/Interactables/Chest.cs
@@ -11,6 +11,11 @@ public class Chest : MonoBehaviour, IInteractable
     [Header("Open chest sprite")]
     [SerializeField] private Sprite openChestSprite;
 
+    [Header("Chest left=true, Chest right=false")]
+    [SerializeField] private bool chestOrientation;
+
+    private int interactNum=0;
+
     void Start()
     {
         foreach (GameObject hiddenObject in hiddenGameObjects)
@@ -21,6 +26,10 @@ public class Chest : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        if (interactNum != 0)
+        {
+            return;
+        }
         if (chestLock == null)
         {
             Debug.Log("chest lock not assigned");
@@ -37,8 +46,19 @@ public class Chest : MonoBehaviour, IInteractable
             if (spriteRenderer != null && openChestSprite != null)
             {
                 spriteRenderer.sprite = openChestSprite;
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.4f, gameObject.transform.position.y, gameObject.transform.position.z);
-                Debug.Log("chest open");
+                if (chestOrientation)
+                {
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x - 0.4f, gameObject.transform.position.y, gameObject.transform.position.z);
+                    Debug.Log("chest open");
+                    
+                }
+                else
+                {
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.4f, gameObject.transform.position.y, gameObject.transform.position.z);
+                    Debug.Log("chest open");
+                }
+                interactNum+=1;
+                   
             }
            
             foreach (GameObject hiddenObject in hiddenGameObjects)
